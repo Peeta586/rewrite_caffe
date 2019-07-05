dd3588dfd7c708cfa94ffc9020018b7f6c7957fa
shiming.li <shiming.li@hirain.com>
2019-07-05 15:22:04 +0800

    caffe.cpp with some error no input file

---
+++ include/caffe/caffe.hpp
@@ -0,0 +1,11 @@
+// caffe.hpp is the header file that you need to include in your code. It wraps
+// all the internal caffe header files into one for simpler inclusion.
+
+#ifndef CAFFE_CAFFE_HPP_
+#define CAFFE_CAFFE_HPP_
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+
+
+#endif // CAFFE_CAFFE_HPP_
---
+++ include/caffe/common.hpp
@@ -0,0 +1,8 @@
+#ifndef CAFFE_COMMON_HPP_
+#define CAFFE_COMMON_HPP_
+
+#include <boost/shared_ptr.hpp>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#endif // CAFFE_COMMON_HPP_
